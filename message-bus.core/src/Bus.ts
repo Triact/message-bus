66fb936ec55fa2354e42ae856555ed3aa463451d
Sander Driesen <sander.driesen@triact.net>
2021-10-10 20:20:49 +0200

    ..

--- message-bus.core/src/Bus.ts
+++ message-bus.core/src/Bus.ts
@@ -49,7 +49,7 @@ export default class Bus implements interfaces.IBus {
         this._transportImplementation.publish(msg, msgType, dest.topic);
     }
 
-    send = <T>(msgCtor: new (...args: any[]) => T, populateMessageCallback: (m: T) => void) => {
+    send = <T>(msgCtor: new (...args: any[]) => T, populateMessageCallback: (m: T) => promise<void) => {
         if (!msgCtor) throw new Error(`Argument 'ctor' cannot be null.`);
         if (!populateMessageCallback) throw new Error(`Argument 'populateMessageCallback' cannot be null`);
         
